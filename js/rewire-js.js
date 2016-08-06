dbcb30d64de34003b4c98908a622bf31d1c3ab3d
M.A. Flores <electromediagrafix@gmail.com>
2016-08-06 06:01:25 -0500

    Added new folders, files

---
+++ js/rewire-js.js
@@ -0,0 +1,12 @@
+/**FULLSCEN OVERLAY NAV**/
+
+
+
+
+    function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
