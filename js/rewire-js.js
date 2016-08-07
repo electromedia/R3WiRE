b72609434c127ea0552c85e36d9a1e9c7c1efaa2
M.A. Flores <electromediagrafix@gmail.com>
2016-08-07 06:02:00 -0500

    Massive changes and add. items.

--- js/rewire-js.js
+++ js/rewire-js.js
@@ -1,12 +1,64 @@
-/**FULLSCEN OVERLAY NAV**/
+/**FULLPAGE OPTIONAL SETTINGS**/
 
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+       //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
 
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
 
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
 
-    function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
+        //Design
+        controlArrows: true,
+        verticalCentered: false,
+        /**sectionsColor : ['#111', '#222', '#111', '#000'], **/
+        paddingTop: '0',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
 
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+
+    });
+});
